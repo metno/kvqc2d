79bc4d357208e2609a0ef6da6bf8c00ba932bd45
Paul Emils Eglitis <paule@met.no>
2010-02-27 23:55:56 +0000

    kvQc2 separated from kvalobs tree
    git-svn-id: https://svn.met.no/kvoss/kvQc2/trunk@1166 422d51e2-5941-41ac-8e65-d5b5931239be

---
+++ src/ProcessControl.h
@@ -0,0 +1,30 @@
+#ifndef _PROCESS_CONTROL_
+#define _PROCESS_CONTROL_
+
+
+#include <kvalobs/kvData.h>
+#include <vector>
+#include <map>
+
+#include "ReadProgramOptions.h"
+
+
+class ProcessControl{
+
+public:
+
+   ProcessControl();
+   bool condition( kvalobs::kvControlInfo controlinfo, std::map<int, unsigned char> vlag );
+   int setter( kvalobs::kvControlInfo &controlinfo, std::map<int, unsigned char> vlag );
+
+   bool true_nibble( kvalobs::kvControlInfo controlinfo, unsigned char nibble );
+
+protected:
+
+private:
+   std::map<unsigned char,int> HexToInt;
+
+};
+
+
+#endif 
