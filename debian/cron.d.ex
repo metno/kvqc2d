39d1d9b91c36697d66c0b9e03cb6d8b1aeda0769
Paul Emils Eglitis <paule@met.no>
2010-04-26 21:23:14 +0000

    generated debian directory with dh_make with some initial control file edits ... ongoing
    git-svn-id: https://svn.met.no/kvoss/kvQc2/trunk@1503 422d51e2-5941-41ac-8e65-d5b5931239be

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the kvqc2 package
+#
+0 4	* * *	root	kvqc2_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(kvqc2):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="kvqc2" command="/usr/bin/kvqc2"
