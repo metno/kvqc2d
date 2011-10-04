1612aa377c72b256bd089bf21f0de27f81df135a
Alexander Bürger <alexander.buerger@met.no>
2011-10-04 13:21:12 +0200

    Read, reformatted, and rearranged Qc2 main code.
    * extracted some helper methods from main
    * removed some stuff that was never used

--- src/Qc2Main.cc
+++ src/Qc2Main.cc
@@ -28,6 +28,7 @@
   with KVALOBS; if not, write to the Free Software Foundation Inc., 
   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
 #include <boost/thread/thread.hpp>
 #include <boost/filesystem/convenience.hpp>
 #include <boost/filesystem/exception.hpp>
@@ -57,136 +58,145 @@ const char* options[][ 2 ] =
 
 ///The kvalobs Qc2 main program.
 
-using namespace std;
-using namespace boost;
+namespace {
 
-int
-main( int argc, char** argv )
+////////////////////////////////////////////////////////////////////////
+
+bool check_rundir()
 {
-  stopwatch SW;
-  CORBA::ORB_ptr orb;
-  PortableServer::POA_ptr poa;
-  ostringstream ost;
-  
-  string dbdriver;
-  miutil::conf::ConfSection *conf = KvApp::getConfiguration();
-  string constr( KvApp::createConnectString() );
-  char *pKv = getenv( "KVALOBS" );
-  bool error;
-
-  milog::LogContext logContext("kvqc2d ...");
-
-  if ( conf )
-  {
-    miutil::conf::ValElementList val = conf->getValue( "database.dbdriver" );
-
-    if ( val.size() == 1 )
-      dbdriver = val[ 0 ].valAsString();
-      std::cout <<  "FROM CONFIGURATION FILE: " << dbdriver << std::endl;
-  }
-
-  //Use postgresql as a last guess.
-  if ( dbdriver.empty() )
-    dbdriver = "pgdriver.so";
-
-  string htmlpath;
-  string logpath_(htmlpath);
-  InitLogger( argc, argv, "kvqc2d", htmlpath );
-  LOGINFO( "kvqc2d: starting ...." );
-
-  filesystem::path rundir( kvPath("localstatedir") + "/run" );
-  std::cout <<   kvPath("localstatedir")  << std::endl;
-  if ( ! boost::filesystem::exists(rundir) ) {
-    try {
-      filesystem::create_directories(rundir);      
+    boost::filesystem::path rundir( kvPath("localstatedir") + "/run" );
+    std::cout << kvPath("localstatedir")  << std::endl;
+    if( !boost::filesystem::exists(rundir) ) {
+        try {
+            boost::filesystem::create_directories(rundir);
+        } catch( boost::filesystem::filesystem_error & e ) {
+            LOGFATAL( e.what() );
+            return false;
+        }
+    } else if( ! boost::filesystem::is_directory(rundir) ) {
+        LOGFATAL( rundir.native_file_string() << " exists but is not a directory" );
+        return false;
     }
-    catch ( filesystem::filesystem_error & e) {
-      LOGFATAL( e.what() );
-      return 1;
+
+    boost::filesystem::path pidfile( dnmi::file::createPidFileName( rundir.native_file_string(), "kvqc2d" ) );
+    bool error = false;
+    if ( dnmi::file::isRunningPidFile( pidfile.native_file_string(), error ) ) {
+        if ( error ) {
+            LOGFATAL( "An error occured while reading the pidfile:" << std::endl
+                      << pidfile.native_file_string() << " remove the file if it exist and"
+                      << std::endl << "kvqc2d is not running. "
+                      << "If it is running and there is problems. Kill kvqc2d and" << std::endl
+                      << "restart it." << std::endl << std::endl );
+            return false;
+        } else {
+            LOGFATAL( "Is kvqc2d allready running?" << std::endl
+                      << "If not remove the pidfile: " << pidfile.native_file_string() );
+            return false;
+        }
     }
-  }
-  else if ( ! filesystem::is_directory(rundir) ) {
-    LOGFATAL( rundir.native_file_string() << "exists but is not a directory" );
-    return 1;
-  }
-  filesystem::path pidfile( dnmi::file::createPidFileName( rundir.native_file_string(), "kvqc2d" ) );
-
-  if ( dnmi::file::isRunningPidFile( pidfile.native_file_string(), error ) )
-  {
-    if ( error )
-    {
-      LOGFATAL( "An error occured while reading the pidfile:" << endl
-                << pidfile.native_file_string() << " remove the file if it exist and"
-                << endl << "kvqc2d is not running. " <<
-                "If it is running and there is problems. Kill kvqc2d and"
-                << endl << "restart it." << endl << endl );
-      return 1;
-    }
-    else
-    {
-      LOGFATAL( "Is kvqc2d allready running?" << endl
-                << "If not remove the pidfile: " << pidfile.native_file_string() );
-      return 1;
-    }
-  }
-
-  Qc2App app( argc, argv, dbdriver, constr, options );
-  
-  if ( !app.isOk() )
-  {
-    LOGFATAL( "FATAL: can't  initialize " << argv[ 0 ] << "!\n" );
-    return 1;
-  }
-
-  orb = app.getOrb();
-  poa = app.getPoa();
-
-
-  PortableServer::POAManager_var pman = app.getPoaMgr();   ///NEW
-  pman->activate();  ///NEW
-
- 
-  app.createPidFile( "kvqc2d" );
-  sleep(1);
-
-  Qc2Work Qc2Work( app, htmlpath );    //commented out while I test program options !!!!
-  boost::thread Qc2Thread( Qc2Work );
-
-  
-  try {
-// This is where all the *InputImpl(app) and AdminImpl( App ) can be reinstalled if ti is needed
-      //app.createPidFile( "Qc2" );
-      orb->run();
-      orb->destroy();
-  }
-  catch ( CORBA::SystemException& ) {
-    LOGFATAL( "Caught CORBA::SystemException." );
-    app.deletePidFile();
-    exit( 1 );
-  }
-  catch ( CORBA::Exception& ) {
-    LOGFATAL( "Caught CORBA::Exception." );
-    app.deletePidFile();
-    exit( 1 );
-  }
-  catch ( omniORB::fatalException & fe ) {
-    LOGFATAL( "Caught omniORB::fatalException:" << endl
-              << "  file: " << fe.file() << endl
-              << "  line: " << fe.line() << endl
-              << "  mesg: " << fe.errmsg() );
-    app.deletePidFile();
-    exit( 1 );
-  }
-  catch ( ... ) {
-    LOGFATAL( "Caught unknown exception." );
-    app.deletePidFile();
-    exit( 1 );
-  }
-
-
-  CERR( "kvqc2d: exit ....\n" );
-  app.deletePidFile();
-
-  return 0;
+    return true;
 }
 
+////////////////////////////////////////////////////////////////////////
+
+std::string find_dbdriver()
+{
+    std::string dbdriver = "pgdriver.so";
+    miutil::conf::ConfSection *conf = KvApp::getConfiguration();
+    if( conf ) {
+        miutil::conf::ValElementList val = conf->getValue( "database.dbdriver" );
+        if( val.size() == 1 )
+            dbdriver = val[ 0 ].valAsString();
+        std::cout <<  "FROM CONFIGURATION FILE: " << dbdriver << std::endl;
+    }
+    
+    if ( dbdriver.empty() ) {
+        // use postgresql as a last guess.
+        dbdriver = "pgdriver.so";
+    }
+
+    return dbdriver;
+}
+
+////////////////////////////////////////////////////////////////////////
+
+struct PidCreateDelete {
+    Qc2App& app;
+    PidCreateDelete(Qc2App& a, const std::string& pidfilename)
+        : app(a)
+        { app.createPidFile(pidfilename); }
+    ~PidCreateDelete()
+        { app.deletePidFile(); }
+};
+
+} // anonymous namespace
+
+// ########################################################################
+
+int main( int argc, char** argv )
+{
+    stopwatch SW;
+    
+    milog::LogContext logContext("kvqc2d ...");
+    
+    string htmlpath;
+    string logpath_(htmlpath);
+    InitLogger( argc, argv, "kvqc2d", htmlpath );
+    LOGINFO( "kvqc2d: starting ...." );
+    
+    if( !check_rundir() )
+        return 1;
+
+    const std::string dbdriver = find_dbdriver();
+    const std::string constr( KvApp::createConnectString() );
+
+    Qc2App app( argc, argv, dbdriver, constr, options );
+  
+    if( !app.isOk() ) {
+        LOGFATAL( "FATAL: can't  initialize " << argv[ 0 ] << "!\n" );
+        return 1;
+    }
+
+    CORBA::ORB_ptr orb = app.getOrb();
+    PortableServer::POA_ptr poa = app.getPoa();
+    PortableServer::POAManager_var pman = app.getPoaMgr();   ///NEW
+    pman->activate();  ///NEW
+
+ 
+    PidCreateDelete pid(app, "kvqc2d");
+    sleep(1);
+
+    Qc2Work Qc2Work( app, htmlpath );    //commented out while I test program options !!!!
+    boost::thread Qc2Thread( Qc2Work );
+
+
+    int exitcode = 0;
+    try {
+        // This is where all the *InputImpl(app) and AdminImpl( App ) can be reinstalled if ti is needed
+        //app.createPidFile( "Qc2" );
+        orb->run();
+        orb->destroy();
+    } catch ( CORBA::SystemException& ) {
+        LOGFATAL( "Caught CORBA::SystemException." );
+        exitcode = 1;
+    } catch ( CORBA::Exception& ) {
+        LOGFATAL( "Caught CORBA::Exception." );
+        exitcode = 1;
+    } catch ( omniORB::fatalException & fe ) {
+        LOGFATAL( "Caught omniORB::fatalException:" << std::endl
+                  << "  file: " << fe.file() << std::endl
+                  << "  line: " << fe.line() << std::endl
+                  << "  mesg: " << fe.errmsg() );
+        exitcode = 1;
+    } catch ( ... ) {
+        LOGFATAL( "Caught unknown exception." );
+        exitcode = 1;
+    }
+
+    if( exitcode == 0 )
+        CERR( "kvqc2d: exit ....\n" );
+
+    // TODO shouldn't there be some kind of Qc2Thread.join() ?
+
+    return exitcode;
+}
