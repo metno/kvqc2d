0f6cfcabad4a8a5d58fb4a4b6fc3d8adfa0a9ac1
Alexander Bürger <alexander.buerger@met.no>
2011-11-16 14:26:02 +0100

    Improved station resolution extraction script.
    * now fetches all pluviometer resolutions, not only 0.1mm and 0.2mm

--- stinfosys-vipp-pluviometer.pl
+++ stinfosys-vipp-pluviometer.pl
@@ -2,45 +2,47 @@
 
 # needs libdbd-pg-perl
 # see http://stinfosys/veil1.html
-# run with " PGPASSWORD=... perl stinfosys-vipp.pl" (space at beginning to avoid bash history)
+# run with " PGPASSWORD=... perl stinfosys-vipp-pluviometer.pl" (space at beginning to avoid bash history)
 
 use DBI;
 use strict;
 
 my $conn = DBI->connect("dbi:Pg:dbname=stinfosys;host=stinfosys;port=5435", "pstinfosys");
 
-sub stationsWithResolution {
-    my ($stationid, $vipp, $infolist, $sep, @row);
-    $vipp = sprintf("%.1f", $_[0]);
-    my $query = $conn->prepare(
-        "SELECT si.stationid FROM sensor_info AS si, equipment AS eq, equipmentmodel AS em "
-        . "WHERE em.description LIKE '%" . $vipp . "mm per vipp%' "
-        . "AND paramid = 105 "
-        . "AND em.modelname = eq.modelname AND si.equipmentid = eq.equipmentid "
-        . "AND ((em.fromtime ISNULL) OR (em.totime ISNULL AND em.fromtime <= now()) "
+my %resolution_stations = ();
+
+my $query = $conn->prepare(
+    "SELECT si.stationid, em.description FROM sensor_info AS si, equipment AS eq, equipmentmodel AS em "
+    . "WHERE em.description LIKE '%mm per vipp%' "
+    . "AND paramid = 105 "
+    . "AND em.modelname = eq.modelname AND si.equipmentid = eq.equipmentid "
+    . "AND ((em.fromtime ISNULL) OR (em.totime ISNULL AND em.fromtime <= now()) "
         . "     OR (em.totime NOTNULL AND now() BETWEEN em.fromtime AND em.totime)) "
-        . "ORDER BY si.stationid"
-        );
-    $query->execute();
+    . "ORDER BY si.stationid"
+    );
+$query->execute();
 
-    $infolist = $vipp;
-    $sep=":";
-    while (@row = $query->fetchrow_array()) {
-        ($stationid) = @row;
-        $infolist .= $sep . sprintf("%d", $stationid);
-        $sep = ",";
+my (@row);
+while (@row = $query->fetchrow_array()) {
+    my ($stationid, $description)= @row;
+    $description =~ /(^|\|)(\d+(\.\d+))mm per vipp/;
+    my $vipp = $2;
+    if( exists $resolution_stations{ $vipp } ) {
+        push( @{$resolution_stations{ $vipp }}, $stationid );
+    } else {
+        $resolution_stations{ $vipp } = [$stationid];
     }
-    undef($query);
-    return $infolist;
 }
 
-my $stations = "stations =";
-my $sep=" ";
-foreach my $mmpv ( (0.1, 0.2) ) {
-    $stations .= $sep . stationsWithResolution($mmpv);
-    $sep=";";
+my $stations_par = "";
+my $stsep = "";
+for my $vipp (sort(keys %resolution_stations)) {
+    $stations_par .= $stsep . $vipp . ":" . join(',', @{$resolution_stations{$vipp}});
+    $stsep .= ";";
 }
-print $stations, "\n";
+
+print "# station list extracted on " . gmtime() . " UTC\n";
+print "stations = " . $stations_par . "\n";
 
 $conn->disconnect();
 $conn = undef;
