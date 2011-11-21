7e8b4a7c5dfa05d7df72c90abe297b56e70bf95a
Alexander Bürger <alexander.buerger@met.no>
2011-11-21 09:06:50 +0100

    Changed stinfosys script after Terje changed the table contents.
    equipmentmodel.description has been reformatted so that the old parser did not
    work any more.

--- stinfosys-vipp-pluviometer.pl
+++ stinfosys-vipp-pluviometer.pl
@@ -13,24 +13,30 @@ my %resolution_stations = ();
 
 my $query = $conn->prepare(
     "SELECT si.stationid, em.description FROM sensor_info AS si, equipment AS eq, equipmentmodel AS em "
-    . "WHERE em.description LIKE '%mm per vipp%' "
+    . "WHERE em.description LIKE '%unit_resolution=mm per vipp%' "
     . "AND paramid = 105 "
     . "AND em.modelname = eq.modelname AND si.equipmentid = eq.equipmentid "
-    . "AND ((em.fromtime ISNULL) OR (em.totime ISNULL AND em.fromtime <= now()) "
-        . "     OR (em.totime NOTNULL AND now() BETWEEN em.fromtime AND em.totime)) "
+    . "AND ((em.fromtime IS NULL) OR (em.totime IS NULL AND em.fromtime <= now()) "
+        . "     OR (em.totime IS NOT NULL AND now() BETWEEN em.fromtime AND em.totime)) "
     . "ORDER BY si.stationid"
     );
 $query->execute();
 
-my (@row);
-while (@row = $query->fetchrow_array()) {
+while (my @row = $query->fetchrow_array()) {
     my ($stationid, $description)= @row;
-    $description =~ /(^|\|)(\d+(\.\d+))mm per vipp/;
-    my $vipp = $2;
-    if( exists $resolution_stations{ $vipp } ) {
-        push( @{$resolution_stations{ $vipp }}, $stationid );
+    if( $description =~ /(^|\|)unit_resolution=mm per vipp($|\|)/ ) {
+        if( $description =~ /(^|\|)resolution=(\d+(\.\d+)?)($|\|)/ ) {
+            my $vipp = $2;
+            if( exists $resolution_stations{ $vipp } ) {
+                push( @{$resolution_stations{ $vipp }}, $stationid );
+            } else {
+                $resolution_stations{ $vipp } = [$stationid];
+            }
+        } else {
+            print "cannot find resolution=... in '$description'\n";
+        }
     } else {
-        $resolution_stations{ $vipp } = [$stationid];
+        print "cannot find unit_resolution=mm per vipp in '$description'\n";
     }
 }
 
