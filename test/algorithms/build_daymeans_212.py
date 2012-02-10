248b3bfb2bf4668f12908432518f9e3cbc94dea3
Alexander Bürger <alexander.buerger@met.no>
2012-02-10 12:12:31 +0100

    Calculate mean of last 30 days for paramid=212 unit test.

--- test/algorithms/build_daymeans_212.py
+++ test/algorithms/build_daymeans_212.py
@@ -2,15 +2,19 @@
 
 import re, sys
 
+MISSING = -32767
+NDAYS = 30
+MAX_MISSING = 3
+
+########################################################################
+# parse text file with normal values
+
 re_stationheader = re.compile(r'^TAM-.*normal for *([0-9]+) - ')
 re_value = re.compile(r' *([0-9]+) +(-?[0-9]+(\.?[0-9]+))')
 
 normals_dat = open(sys.argv[1], 'r')
-normals_cpp = open(sys.argv[2], 'w')
 
-print >> normals_cpp, "{\nstd::ostringstream sql;"
-
-last_stationid = -1
+stations = {}
 for line in normals_dat:
     m = re_stationheader.match(line)
     if m:
@@ -22,18 +26,45 @@ for line in normals_dat:
     m = re_value.match(line)
     if not m:
         continue
+    if not stations.has_key(stationid):
+        stations[stationid] = [MISSING] * 365
     
     day = int(m.group(1))
     normal = float(m.group(2))
 
-    if stationid != last_stationid and last_stationid >= 0:
-        print >> normals_cpp, "ASSERT_NO_THROW(db->exec(sql.str()));\nsql.str("");"
-    last_stationid = stationid
-    print >> normals_cpp, "sql << \"INSERT INTO statistical_reference_values VALUES(%5d,212,%3d,'ref_value',%.1f);\";" % (stationid, day, normal)
+    stations[stationid][day-1] = normal
 
-if stationid != -1:
-    print >> normals_cpp, "ASSERT_NO_THROW(db->exec(sql.str()));\nsql.str("");"
+normals_dat.close()
 
+########################################################################
+# transform to arithmetic mean of the last 30 days
+for stationid in sorted(stations.iterkeys()):
+    normals_file = stations[stationid]
+    normals = [MISSING] * 365
+    for day0 in range(365):
+        day1 = day0 + 1
+        if day0 < NDAYS:
+            values = normals_file[365+day1-NDAYS:365] + normals_file[:day1]
+        else:
+            values = normals_file[day1-NDAYS:day1]
+        n_missing = values.count(MISSING)
+        if n_missing > MAX_MISSING:
+            print "no normal for station=%5d day%3d" % (stationid, day1)
+            continue
+        values = filter(lambda v: v != MISSING, values)
+        normals[day0] = sum(values) / len(values)
+    stations[stationid] = normals
+
+########################################################################
+# write normals to execute SQL statements in unit tests
+
+normals_cpp = open(sys.argv[2], 'w')
+print >> normals_cpp, "{\nstd::ostringstream sql;"
+for stationid in sorted(stations.iterkeys()):
+    for day0, normal in enumerate(stations[stationid]):
+        if normal != MISSING:
+            print >> normals_cpp, "sql << \"INSERT INTO statistical_reference_values VALUES(%5d,212,%3d,'ref_value',%.1f);\";" % (stationid, day0+1, normal)
+    print >> normals_cpp, "ASSERT_NO_THROW(db->exec(sql.str()));\nsql.str(\"\");"
 print >> normals_cpp, """
 }
 
@@ -42,6 +73,4 @@ print >> normals_cpp, """
 // buffer-read-only: t
 // End:
 """
-
 normals_cpp.close()
-normals_dat.close()
